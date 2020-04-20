c24da27f6e4beba99c0afaf863e97800e6a12969
Matt <mqunell@gmail.com>
2020-04-19 22:46:15 -0700

    outline routes

--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
 
 
+// POST '/api/movies'
+router.route('/movies').post((req, res) => {
+});
+
+
 // GET '/api/movies'
-router.route('/').get((req, res) => {
-	res.send('Hello world!');
-	console.log('GET tracked');
+router.route('/movies').get((req, res) => {
+});
+
+
+// GET '/api/movies/:id'
+router.route('/movies/:id').get((req, res) => {
+});
+
+
+// PUT '/api/movies/:id'
+router.route('/movies/:id').put((req, res) => {
+});
+
+
+// DELETE '/api/movies/:id'
+router.route('/movies/:id').delete((req, res) => {
 });
 
 
