a91c102ced7f6564948e05f02f399f62b26abae6
Matt <mqunell@gmail.com>
2020-04-25 18:29:48 -0700

    prepare App.js and App.css; add Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
 	return (
-	<div className="App">
-		<header className="App-header">
-			<p>
-				Edit <code>src/App.js</code> and save to reload.
-			</p>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-		</header>
-	</div>
+		<p>Hello, React</p>
 	);
 }
 
