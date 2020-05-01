4d40cd12736175ea2e9a068f2c18b105761354ed
Matt <mqunell@gmail.com>
2020-04-30 18:12:14 -0700

    remove collapsing navbar
    Since Bootstrap's collapsing navbar requires jQuery and this one is only ~330px wide, collapsing functionality was removed

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,26 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 			<Link to="/" className="navbar-brand">Movies Library</Link>
 
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
-
-			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-				<ul className="navbar-nav">
-					<li className="navbar-item">
-						<Link to="/library" className="nav-link">Library</Link>
-					</li>
-					<li className="navbar-item">
-						<Link to="/add" className="nav-link">Add</Link>
-					</li>
-					<li className="navbar-item">
-						<Link to="/about" className="nav-link">About</Link>
-					</li>
-				</ul>
-			</div>
+			<ul className="navbar-nav">
+				<li className="navbar-item">
+					<Link to="/library" className="nav-link">Library</Link>
+				</li>
+				<li className="navbar-item">
+					<Link to="/add" className="nav-link">Add</Link>
+				</li>
+				<li className="navbar-item">
+					<Link to="/about" className="nav-link">About</Link>
+				</li>
+			</ul>
 		</nav>
 	)
 }
