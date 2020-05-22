9d80dcefe1b23218a4b58f4ce23d2921b0548715
Matt <mqunell@gmail.com>
2020-05-22 15:36:33 -0700

    refactor MovieCard.jsx with React hover
    Testing using onMouseEnter/state for the hover animation instead of CSS

--- frontend/src/components/Library.jsx
+++ frontend/src/components/Library.jsx
@@ -1,67 +1,6 @@
 import React, { Component } from 'react'
+import MovieCard from './MovieCard'
 import axios from 'axios'
-
-
-// Functional React component (no state or lifecycle methods)
-// Good for accepting props and returning JSX
-const MovieCard = props => (
-	<div className="movie_card">
-		<img className="movie_card_image" src={props.movie.poster} alt="Cover art"/>
-		<div className="movie_card_details">
-			<div className="movie_card_details_line">
-				<p>{props.movie.title.length > 40 ? props.movie.title.slice(0, 40) + '...' : props.movie.title}</p>
-				<p>{props.movie.year}</p>
-			</div>
-
-			<div className="movie_card_details_line">
-				<p>{props.movie.rating}</p>
-				<p>{props.movie.runtime}</p>
-				<p>{props.movie.metacritic}/100</p>
-			</div>
-
-			<p>{props.movie.plot}</p>
-
-			<div className="formats_container">
-				<MovieFormatItem data={{
-					formatString: 'DVD',
-					hasFormat: props.movie.formats.includes('1'),
-					formatIcon: 'compact-disc'
-				}}/>
-
-				<MovieFormatItem data={{
-					formatString: 'Bluray',
-					hasFormat: props.movie.formats.includes('2'),
-					formatIcon: 'compact-disc'
-				}}/>
-
-				<MovieFormatItem data={{
-					formatString: '4K',
-					hasFormat: props.movie.formats.includes('3'),
-					formatIcon: 'compact-disc'
-				}}/>
-
-				<MovieFormatItem data={{
-					formatString: 'Digital',
-					hasFormat: props.movie.formats.includes('4'),
-					formatIcon: 'hdd'
-				}}/>
-
-				<MovieFormatItem data={{
-					formatString: 'Stream',
-					hasFormat: props.movie.formats.includes('5'),
-					formatIcon: 'cloud'
-				}}/>
-			</div>
-		</div>
-	</div>
-)
-
-const MovieFormatItem = props => (
-	<div className={'format_item format_' + props.data.formatString.toLowerCase() + (props.data.hasFormat ? ' has_format' : '')}>
-		<i className={'fas fa-' + props.data.formatIcon}></i>
-		<p>{props.data.formatString}</p>
-	</div>
-)
 
 
 export default class Library extends Component {
@@ -101,7 +40,7 @@ export default class Library extends Component {
 			}
 
 			// Use MongoDB ID for React key
-			output.push(<MovieCard movie={movie} key={movie._id}/>)
+			output.push(<MovieCard movie={movie} key={movie._id} />)
 		})
 
 		return output
