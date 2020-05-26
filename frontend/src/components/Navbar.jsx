18a03b7ec0c646ac5e3f46450f4e37b9d1899bc1
Matt <mqunell@gmail.com>
2020-05-25 19:58:18 -0700

    hide the expanded navbar when clicking a link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap'
 
+function hideNavbar() {
+	document.getElementsByClassName('navbar-toggler')[0].click()
+}
+
 const MyNavbar = () => (
 	<Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
 		<Navbar.Brand as={NavLink} to="/">Movies Library</Navbar.Brand>
@@ -10,9 +14,9 @@ const MyNavbar = () => (
 		
 		<Navbar.Collapse id="responsive-navbar-nav">
 			<Nav className="mr-auto">
-				<Nav.Link as={NavLink} to="/library">Library</Nav.Link>
-				<Nav.Link as={NavLink} to="/add">Add</Nav.Link>
-				<Nav.Link as={NavLink} to="/about">About</Nav.Link>
+				<Nav.Link as={NavLink} to="/library" onClick={hideNavbar}>Library</Nav.Link>
+				<Nav.Link as={NavLink} to="/add" onClick={hideNavbar}>Add</Nav.Link>
+				<Nav.Link as={NavLink} to="/about" onClick={hideNavbar}>About</Nav.Link>
 			</Nav>
 		</Navbar.Collapse>
 	</Navbar>
