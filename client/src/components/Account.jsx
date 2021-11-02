03d9cb3f9c4cf3bf1afc8258beb60eedda8bca0e
Matt <mqunell@gmail.com>
2021-11-01 18:29:31 -0700

    refactor LogIn and CreateAccount; add Account background

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -2,17 +2,10 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useAlert } from '../contexts/AlertContext';
 
+/* eslint-disable jsx-a11y/anchor-is-valid */
 export default function Account({ setUsername }) {
-	const addAlert = useAlert();
-
 	const [activeForm, setActiveForm] = useState('login');
 	const [animateOut, setAnimateOut] = useState(false);
-
-	const [createEmail, setCreateEmail] = useState('');
-	const [createPasswordOne, setCreatePasswordOne] = useState('');
-	const [createPasswordTwo, setCreatePasswordTwo] = useState('');
-	const [loginEmail, setLoginEmail] = useState('');
-	const [loginPassword, setLoginPassword] = useState('');
 
 	const toggleForm = () => {
 		setAnimateOut(true);
@@ -22,6 +15,88 @@ export default function Account({ setUsername }) {
 			setActiveForm(activeForm === 'login' ? 'create' : 'login');
 		}, 250);
 	};
+
+	return (
+		<div id="account">
+			{activeForm === 'login' ? (
+				<LogIn
+					animateOut={animateOut}
+					toggleForm={toggleForm}
+					setUsername={setUsername}
+				/>
+			) : (
+				<CreateAccount
+					animateOut={animateOut}
+					toggleForm={toggleForm}
+					setUsername={setUsername}
+				/>
+			)}
+		</div>
+	);
+}
+
+function LogIn({ animateOut, toggleForm, setUsername }) {
+	const addAlert = useAlert();
+
+	const [loginEmail, setLoginEmail] = useState('');
+	const [loginPassword, setLoginPassword] = useState('');
+
+	const onSubmitLogin = (e) => {
+		e.preventDefault();
+
+		// Form's "required" properties handle basic validation
+
+		const email = loginEmail;
+		const password = loginPassword;
+
+		const loginUser = { email, password };
+
+		axios
+			.post('/api/users/login', loginUser)
+			.then(() => {
+				addAlert({ type: 'success', text: 'Logging in' });
+				setUsername(email);
+			})
+			.catch((error) => addAlert({ type: 'danger', text: error.response.data }));
+	};
+
+	return (
+		<form className={animateOut && 'animateOut'} onSubmit={onSubmitLogin}>
+			<input
+				type="email"
+				className="form-control"
+				id="login_email"
+				aria-describedby="emailHelp"
+				placeholder="Email"
+				required
+				onChange={(e) => setLoginEmail(e.target.value)}
+			/>
+			<input
+				type="password"
+				className="form-control"
+				id="login_password"
+				placeholder="Password"
+				required
+				onChange={(e) => setLoginPassword(e.target.value)}
+			/>
+			{/* <p>Forgot password</p> */}
+			<button type="submit" className="btn btn-primary" id="login_submit">
+				Log In
+			</button>
+			<hr />
+			<a href="#" onClick={toggleForm}>
+				Create Account
+			</a>
+		</form>
+	);
+}
+
+function CreateAccount({ animateOut, toggleForm, setUsername }) {
+	const addAlert = useAlert();
+
+	const [createEmail, setCreateEmail] = useState('');
+	const [createPasswordOne, setCreatePasswordOne] = useState('');
+	const [createPasswordTwo, setCreatePasswordTwo] = useState('');
 
 	const onSubmitCreate = (e) => {
 		e.preventDefault();
@@ -49,87 +124,40 @@ export default function Account({ setUsername }) {
 			.catch((error) => addAlert({ type: 'danger', text: error.response.data }));
 	};
 
-	const onSubmitLogin = (e) => {
-		e.preventDefault();
-
-		// Form's "required" properties handle basic validation
-
-		const email = loginEmail;
-		const password = loginPassword;
-
-		const loginUser = { email, password };
-
-		axios
-			.post('/api/users/login', loginUser)
-			.then(() => {
-				addAlert({ type: 'success', text: 'Logging in' });
-				setUsername(email);
-			})
-			.catch((error) => addAlert({ type: 'danger', text: error.response.data }));
-	};
-
 	return (
-		<div id="account">
-			{activeForm === 'login' ? (
-				<form className={animateOut && 'animateOut'} onSubmit={onSubmitLogin}>
-					<input
-						type="email"
-						className="form-control"
-						id="login_email"
-						aria-describedby="emailHelp"
-						placeholder="Email"
-						required
-						onChange={(e) => setLoginEmail(e.target.value)}
-					/>
-					<input
-						type="password"
-						className="form-control"
-						id="login_password"
-						placeholder="Password"
-						required
-						onChange={(e) => setLoginPassword(e.target.value)}
-					/>
-					{/* <p>Forgot password</p> */}
-					<button type="submit" className="btn btn-primary" id="login_submit">
-						Log In
-					</button>
-					<hr />
-					<p onClick={toggleForm}>switch view</p>
-				</form>
-			) : (
-				<form className={animateOut && 'animateOut'} onSubmit={onSubmitCreate}>
-					<input
-						type="email"
-						className="form-control"
-						id="create_email"
-						aria-describedby="emailHelp"
-						placeholder="Email"
-						required
-						onChange={(e) => setCreateEmail(e.target.value)}
-					/>
-					<input
-						type="password"
-						className="form-control"
-						id="create_password_1"
-						placeholder="Password"
-						required
-						onChange={(e) => setCreatePasswordOne(e.target.value)}
-					/>
-					<input
-						type="password"
-						className="form-control"
-						id="create_password_2"
-						placeholder="Confirm password"
-						required
-						onChange={(e) => setCreatePasswordTwo(e.target.value)}
-					/>
-					<button type="submit" className="btn btn-primary" id="create_submit">
-						Create Account
-					</button>
-					<hr />
-					<p onClick={toggleForm}>switch view</p>
-				</form>
-			)}
-		</div>
+		<form className={animateOut && 'animateOut'} onSubmit={onSubmitCreate}>
+			<input
+				type="email"
+				className="form-control"
+				id="create_email"
+				aria-describedby="emailHelp"
+				placeholder="Email"
+				required
+				onChange={(e) => setCreateEmail(e.target.value)}
+			/>
+			<input
+				type="password"
+				className="form-control"
+				id="create_password_1"
+				placeholder="Password"
+				required
+				onChange={(e) => setCreatePasswordOne(e.target.value)}
+			/>
+			<input
+				type="password"
+				className="form-control"
+				id="create_password_2"
+				placeholder="Confirm password"
+				required
+				onChange={(e) => setCreatePasswordTwo(e.target.value)}
+			/>
+			<button type="submit" className="btn btn-primary" id="create_submit">
+				Create Account
+			</button>
+			<hr />
+			<a href="#" onClick={toggleForm}>
+				Log In
+			</a>
+		</form>
 	);
 }
