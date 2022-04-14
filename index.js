30d3438b45110c0f688b2c43274be715fc54199c
Matt <mqunell@gmail.com>
2022-04-14 00:29:27 -0700

    fix deployment bugs

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use('/api', require('./apiRouter'));
 
 // Serve the frontend
 if (NODE_ENV === 'production' || NODE_ENV === 'staging') {
-	app.use(express.static('../client/build'));
+	app.use(express.static('./client/build'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+		res.sendFile(path.join(__dirname, './client/build/index.html'));
 	});
 }
 
